4eadeeceba987ffa72a7bf7c855d3b26452b8ad6
Martijn Keesmaat <martijnpk@ziggo.nl>
2019-03-08 16:56:02 +0100

    Added ESlint

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+	'env': {
+		'browser': true,
+		'commonjs': true,
+		'es6': true
+	},
+	'extends': 'eslint:recommended',
+	'globals': {
+		'Atomics': 'readonly',
+		'SharedArrayBuffer': 'readonly'
+	},
+
+	'rules': {
+		'indent': [
+			'error',
+			'tab'
+		],
+		'linebreak-style': [
+			'error',
+			'unix'
+		],
+		'quotes': [
+			'error',
+			'single'
+		],
+		'semi': [
+			'error',
+			'always'
+		]
+	}
+};
--- routes/index.js
+++ routes/index.js
@@ -1,63 +1,63 @@
 'use strict';
 const express = require('express');
-const router = express.Router();
+var router = express.Router();
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-  res.render('index', {
-    title: 'Dating app',
-    people: [{
-      "id": 1,
-      "name": "Leanne Graham",
-      "username": "Bret",
-      "email": "Sincere@april.biz",
-      "address": {
-        "street": "Kulas Light",
-        "suite": "Apt. 556",
-        "city": "Gwenborough",
-        "zipcode": "92998-3874",
-        "geo": {
-          "lat": "-37.3159",
-          "lng": "81.1496"
-        }
-      },
-      "phone": "1-770-736-8031 x56442",
-      "website": "hildegard.org",
-      "company": {
-        "name": "Romaguera-Crona",
-        "catchPhrase": "Multi-layered client-server neural-net",
-        "bs": "harness real-time e-markets"
-      }
-    }, {
-      "id": 2,
-      "name": "Ervin Howell",
-      "username": "Antonette",
-      "email": "Shanna@melissa.tv",
-      "address": {
-        "street": "Victor Plains",
-        "suite": "Suite 879",
-        "city": "Wisokyburgh",
-        "zipcode": "90566-7771",
-        "geo": {
-          "lat": "-43.9509",
-          "lng": "-34.4618"
-        }
-      },
-      "phone": "010-692-6593 x09125",
-      "website": "anastasia.net",
-      "company": {
-        "name": "Deckow-Crist",
-        "catchPhrase": "Proactive didactic contingency",
-        "bs": "synergize scalable supply-chains"
-      }
-    }, ]
-  });
+router.get('/', function (req, res) {
+	res.render('index', {
+		title: 'Dating app',
+		people: [{
+			'id': 1,
+			'name': 'Leanne Graham',
+			'username': 'Bret',
+			'email': 'Sincere@april.biz',
+			'address': {
+				'street': 'Kulas Light',
+				'suite': 'Apt. 556',
+				'city': 'Gwenborough',
+				'zipcode': '92998-3874',
+				'geo': {
+					'lat': '-37.3159',
+					'lng': '81.1496'
+				}
+			},
+			'phone': '1-770-736-8031 x56442',
+			'website': 'hildegard.org',
+			'company': {
+				'name': 'Romaguera-Crona',
+				'catchPhrase': 'Multi-layered client-server neural-net',
+				'bs': 'harness real-time e-markets'
+			}
+		}, {
+			'id': 2,
+			'name': 'Ervin Howell',
+			'username': 'Antonette',
+			'email': 'Shanna@melissa.tv',
+			'address': {
+				'street': 'Victor Plains',
+				'suite': 'Suite 879',
+				'city': 'Wisokyburgh',
+				'zipcode': '90566-7771',
+				'geo': {
+					'lat': '-43.9509',
+					'lng': '-34.4618'
+				}
+			},
+			'phone': '010-692-6593 x09125',
+			'website': 'anastasia.net',
+			'company': {
+				'name': 'Deckow-Crist',
+				'catchPhrase': 'Proactive didactic contingency',
+				'bs': 'synergize scalable supply-chains'
+			}
+		},]
+	});
 });
 
-router.get('/profile', function (req, res, next) {
-  res.render('profile', {
-    title: 'Profile'
-  });
+router.get('/profile', function (req, res) {
+	res.render('profile', {
+		title: 'Profile'
+	});
 });
 
 module.exports = router;
