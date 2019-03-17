cade55049b35f22b40734ad88d645b5ef8601e56
Martijn Keesmaat <martijnpk@ziggo.nl>
2019-03-17 11:38:41 +0100

    Fix express bug

--- app.js
+++ app.js
@@ -21,10 +21,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
-app.use(next => next(createError(404)));
+app.use((req, res, next) => {
+	next(createError(404));
+});
 
 // error handler
-app.use((err, req, res) => {
+app.use((err, req, res, next) => {
 	// set locals, only providing error in development
 	res.locals.message = err.message;
 	res.locals.error = req.app.get('env') === 'development' ? err : {};
