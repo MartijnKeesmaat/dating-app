9ee0f2cc50218f5f4aabfec88e7b1969092fe3f9
Martijn Keesmaat <martijnpk@ziggo.nl>
2019-03-20 13:49:17 +0100

    ESlint was bugging me

--- public/scripts/ice-breaker.js
+++ public/scripts/ice-breaker.js
@@ -1,54 +1,54 @@
 (function () {
-  'use strict';
+	'use strict';
 
-  var body = document.body;
+	var body = document.body;
 
-  // run code
-  enableJS();
+	// run code
+	enableJS();
 
-  // functions declarations  
-  function enableJS() {
-    body.classList.replace('no-js', 'js-enabled');
-  }
-  function $(element) {
-    return document.querySelector(element);
-  }
-  function $$(elements) {
-    return document.querySelectorAll(elements);
-  }
+	// functions declarations  
+	function enableJS() {
+		body.classList.replace('no-js', 'js-enabled');
+	}
+	function $(element) {
+		return document.querySelector(element);
+	}
+	function $$(elements) {
+		return document.querySelectorAll(elements);
+	}
 
-  // Ice-breaker
-  $$('.icebreaker .ice-q').forEach(i => i.addEventListener('click', nextQuestion, false));
-  const sets = $$('fieldset');
-  let current = 0;
-  let currentSet = sets[current];
-
-
-  // Show next set
-  function nextQuestion() {
-    current++;
-    if (current < sets.length) {
-      currentSet.classList.remove('active');
-      currentSet = sets[current];
-      currentSet.classList.add('active');
-    }
-    if (current + 1 >= sets.length) {
-      $('.icebreaker h4').remove();
-      $('.icebreaker h2').textContent = '🎉 Ice-breaker';
-    }
-    // let currentProgress = $('progress').value;
-    $('progress').value = $('progress').value + 25;
-  }
-
-  // Close icebreaker
-  $$('.icebreaker__close').forEach(i => i.addEventListener('click', closeBreaker, false));
-  function closeBreaker() {
-    body.classList.remove('is-icebreaker-active');
-  }
+	// Ice-breaker
+	$$('.icebreaker .ice-q').forEach(i => i.addEventListener('click', nextQuestion, false));
+	const sets = $$('fieldset');
+	let current = 0;
+	let currentSet = sets[current];
 
 
-  $$('.icebreaker__open').forEach(i => i.addEventListener('click', openBreaker, false));
-  function openBreaker() {
-    body.classList.add('is-icebreaker-active');
-  }
+	// Show next set
+	function nextQuestion() {
+		current++;
+		if (current < sets.length) {
+			currentSet.classList.remove('active');
+			currentSet = sets[current];
+			currentSet.classList.add('active');
+		}
+		if (current + 1 >= sets.length) {
+			$('.icebreaker h4').remove();
+			$('.icebreaker h2').textContent = '🎉 Ice-breaker';
+		}
+		// let currentProgress = $('progress').value;
+		$('progress').value = $('progress').value + 25;
+	}
+
+	// Close icebreaker
+	$$('.icebreaker__close').forEach(i => i.addEventListener('click', closeBreaker, false));
+	function closeBreaker() {
+		body.classList.remove('is-icebreaker-active');
+	}
+
+
+	$$('.icebreaker__open').forEach(i => i.addEventListener('click', openBreaker, false));
+	function openBreaker() {
+		body.classList.add('is-icebreaker-active');
+	}
 })();
