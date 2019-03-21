2dbe6175cd045514ad7826ad83333e3e8a256977
Martijn Keesmaat <martijnpk@ziggo.nl>
2019-03-21 17:24:04 +0100

    Inserts document into collection

--- app.js
+++ app.js
@@ -21,11 +21,26 @@ MongoClient.connect(url, function (err, db) {
 	assert.equal(null, err);
 	console.log('Connected successfully to server');
 
-	db.close();
+	insertDocuments(db, function () {
+		db.close();
+	});
 });
 
 
-
+var insertDocuments = function (db, callback) {
+	// Get the documents collection
+	var collection = db.collection('documents');
+	// Insert some documents
+	collection.insertMany([
+		{ a: 1 }, { a: 2 }, { a: 3 }
+	], function (err, result) {
+		assert.equal(err, null);
+		assert.equal(3, result.result.n);
+		assert.equal(3, result.ops.length);
+		console.log('Inserted 3 documents into the collection');
+		callback(result);
+	});
+};
 
 
 
