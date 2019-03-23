73dcfe74aa7a8f39fd8f320562cd7b841077001b
Martijn Keesmaat <martijnpk@ziggo.nl>
2019-03-23 10:47:07 +0100

    Comments out insert function

--- app.js
+++ app.js
@@ -27,51 +27,34 @@ MongoClient.connect(url, function (err, db) {
 	// 	});
 	// });
 
-	insertDocuments(db, function () {
-		updateDocument(db, function () {
-			removeDocument(db, function () {
-				db.close();
-			});
-		});
+	findDocuments(db, function () {
+		db.close();
 	});
 });
 
 
-var insertDocuments = function (db, callback) {
-	// Get the documents collection
-	var collection = db.collection('documents');
-	// Insert some documents
-	collection.insertMany([
-		{ a: 1 }, { a: 2 }, { a: 3 }
-	], function (err, result) {
-		assert.equal(err, null);
-		assert.equal(3, result.result.n);
-		assert.equal(3, result.ops.length);
-		console.log('Inserted 3 documents into the collection');
-		callback(result);
-	});
-};
-
-
-// Find all arrs
-// var findDocuments = function (db, callback) {
+// var insertDocuments = function (db, callback) {
 // 	// Get the documents collection
 // 	var collection = db.collection('documents');
-// 	// Find some documents
-// 	collection.find({}).toArray(function (err, docs) {
+// 	// Insert some documents
+// 	collection.insertMany([
+// 		{ a: 1 }, { a: 2 }, { a: 3 }
+// 	], function (err, result) {
 // 		assert.equal(err, null);
-// 		console.log('Found the following records');
-// 		console.log(docs);
-// 		callback(docs);
+// 		assert.equal(3, result.result.n);
+// 		assert.equal(3, result.ops.length);
+// 		console.log('Inserted 3 documents into the collection');
+// 		callback(result);
 // 	});
 // };
 
-// Find specific key value pair
+
+// Find all arrs
 var findDocuments = function (db, callback) {
 	// Get the documents collection
 	var collection = db.collection('documents');
 	// Find some documents
-	collection.find({ 'a': 3 }).toArray(function (err, docs) {
+	collection.find({}).toArray(function (err, docs) {
 		assert.equal(err, null);
 		console.log('Found the following records');
 		console.log(docs);
@@ -79,31 +62,44 @@ var findDocuments = function (db, callback) {
 	});
 };
 
-var updateDocument = function (db, callback) {
-	// Get the documents collection
-	var collection = db.collection('documents');
-	// Update document where a is 2, set b equal to 1
-	collection.updateOne({ a: 2 }
-		, { $set: { b: 1 } }, function (err, result) {
-			assert.equal(err, null);
-			assert.equal(1, result.result.n);
-			console.log('Updated the document with the field a equal to 2');
-			callback(result);
-		});
-};
+// Find specific key value pair
+// var findDocuments = function (db, callback) {
+// 	// Get the documents collection
+// 	var collection = db.collection('documents');
+// 	// Find some documents
+// 	collection.find({ 'a': 3 }).toArray(function (err, docs) {
+// 		assert.equal(err, null);
+// 		console.log('Found the following records');
+// 		console.log(docs);
+// 		callback(docs);
+// 	});
+// };
+
+// var updateDocument = function (db, callback) {
+// 	// Get the documents collection
+// 	var collection = db.collection('documents');
+// 	// Update document where a is 2, set b equal to 1
+// 	collection.updateOne({ a: 2 }
+// 		, { $set: { b: 1 } }, function (err, result) {
+// 			assert.equal(err, null);
+// 			assert.equal(1, result.result.n);
+// 			console.log('Updated the document with the field a equal to 2');
+// 			callback(result);
+// 		});
+// };
 
 
-var removeDocument = function (db, callback) {
-	// Get the documents collection
-	var collection = db.collection('documents');
-	// Delete document where a is 3
-	collection.deleteOne({ a: 3 }, function (err, result) {
-		assert.equal(err, null);
-		assert.equal(1, result.result.n);
-		console.log('Removed the document with the field a equal to 3');
-		callback(result);
-	});
-};
+// var removeDocument = function (db, callback) {
+// 	// Get the documents collection
+// 	var collection = db.collection('documents');
+// 	// Delete document where a is 3
+// 	collection.deleteOne({ a: 3 }, function (err, result) {
+// 		assert.equal(err, null);
+// 		assert.equal(1, result.result.n);
+// 		console.log('Removed the document with the field a equal to 3');
+// 		callback(result);
+// 	});
+// };
 
 
 // view engine setup
