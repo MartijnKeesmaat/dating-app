f63920035ce46d9c013cf06035297a1e9f278e42
Martijn Keesmaat <martijnpk@ziggo.nl>
2019-03-23 12:35:32 +0100

    Styling overview page

--- server/data.js
+++ server/data.js
@@ -2,6 +2,10 @@ const users = [{
 	'id': 1,
 	'name': 'Leanne Graham',
 	'username': 'Bret',
+	'images': {
+		'display': '/images/users/1/display.jpg'
+	},
+	'job': 'UI designer',
 	'email': 'Sincere@april.biz',
 	'address': {
 		'street': 'Kulas Light',
@@ -20,11 +24,16 @@ const users = [{
 		'catchPhrase': 'Multi-layered client-server neural-net',
 		'bs': 'harness real-time e-markets'
 	}
-}, {
+},
+{
 	'id': 2,
 	'name': 'Ervin Howell',
 	'username': 'Antonette',
 	'email': 'Shanna@melissa.tv',
+	'images': {
+		'display': '/images/users/2/display.jpg'
+	},
+	'job': 'Photographer',
 	'address': {
 		'street': 'Victor Plains',
 		'suite': 'Suite 879',
